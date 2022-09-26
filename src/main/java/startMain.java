e64fa352b8d7668b31f4258ba345903e09d07a68
QuocY <zero.votinh.mh@gmail.com>
2022-09-26 09:01:25 +0700

    review

--- src/main/java/startMain.java
+++ src/main/java/startMain.java
@@ -4,6 +4,7 @@ import io.vertx.core.Vertx;
 public class startMain {
 
   public static void main(String[] args) {
+    // review
     Vertx vertx = Vertx.vertx();
     vertx.deployVerticle(new startVerticle());
   }
